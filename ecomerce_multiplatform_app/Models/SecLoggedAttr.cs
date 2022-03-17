bd85d3e687e7fb825c839d65240b9e5e38a5be96
JK Nguyen <khoanguyenjk.108@gmail.com>
2022-03-17 16:54:42 +0700

    Add project files.

---
+++ ecomerce_multiplatform_app/Models/SecLoggedAttr.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ecomerce_multiplatform_app.Models
+{
+    public partial class SecLoggedAttr
+    {
+        public Guid Id { get; set; }
+        public DateTime? CreateTs { get; set; }
+        public string? CreatedBy { get; set; }
+        public Guid? EntityId { get; set; }
+        public string? Name { get; set; }
+
+        public virtual SecLoggedEntity? Entity { get; set; }
+    }
+}
---
+++ ecomerce_multiplatform_app/Models/SysDbChangelog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ecomerce_multiplatform_app.Models
+{
+    public partial class SysDbChangelog
+    {
+        public string ScriptName { get; set; } = null!;
+        public DateTime? CreateTs { get; set; }
+        public int? IsInit { get; set; }
+    }
+}
